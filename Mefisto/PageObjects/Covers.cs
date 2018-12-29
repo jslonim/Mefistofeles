24c6dc80f341cffbe326673ff83bd2e0126d5435
jslonim <slonim.julian@gmal.com>
2018-12-29 15:49:05 -0300

    2 pages of SCP

---
+++ Mefisto/PageObjects/Covers.cs
@@ -0,0 +1,18 @@
+﻿using Repositories.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mefistofeles.PageObjects
+{
+    class Covers
+    {
+        
+        //public List<Match> GetCoversPercentages(List<Match> matches)
+        //{
+
+        //}
+    }
+}
--- Mefisto/PageObjects/SportsChatPlace/SportsChatPlace.cs
+++ Mefisto/PageObjects/SportsChatPlace/SportsChatPlace.cs
@@ -12,8 +12,7 @@ using System.Threading.Tasks;
 namespace Mefistofeles.PageObjects
 {
     public class SportsChatPlace : PageObjectBase
-    {
-        private string URL = "https://sportschatplace.com/{1}-picks/";
+    {      
         By entries = By.CssSelector(".sports-a-cell");
         By entryTitle = By.CssSelector("a");
         By matchPick = By.CssSelector("h3.block");
@@ -22,43 +21,51 @@ namespace Mefistofeles.PageObjects
 
         public List<Match> FillMatchesPicks(List<Match> matchList, SportsEnum sport)
         {
-            List<string> entriesLinks = new List<string>();
             List<Match> existingMatches = new List<Match>();
-            URL = URL.Replace("{1}", sport.ToString());
-            browser.Navigate().GoToUrl(URL);
-            WaitForPageLoad(20);
 
-            foreach (var entry in browser.FindElements(entries))
+            for (int i = 1; i <= 2; i++)
             {
-                string[] title = entry.FindElements(entryTitle)[0].Text.Split(new string[] { "-", "NHL", "NBA", "NFL" , "MBL" , "Soccer" }, StringSplitOptions.None);
-                title[0] = title[0].Trim();
-                title[1] = title[1].Trim();
-                matchList[0].MatchDttm.Date.ToString("MM/dd/yy");
-                bool matchExsit = matchList.Any(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy"));
-
-                if (matchExsit)
-                {
-                    entriesLinks.Add(entry.FindElements(entryTitle)[0].GetAttribute("href"));
-                    existingMatches.Add(matchList.First(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy")));
-                }
-            }
-
-            foreach (var link in entriesLinks)
-            {
-                browser.Navigate().GoToUrl(link);
-                string[] title = browser.FindElements(articleTitle)[0].Text.Split(new string[] { "-", "NHL", "NBA", "NFL", "MBL", "Soccer" }, StringSplitOptions.None);
-                title[0] = title[0].Trim();
-                title[1] = title[1].Trim();
+                string URL = "https://sportschatplace.com/{1}-picks/?page={2}";
+                List<string> entriesLinks = new List<string>();
+                URL = URL.Replace("{1}", sport.ToString());
+                URL = URL.Replace("{2}", i.ToString());
+                browser.Navigate().GoToUrl(URL);
                 WaitForPageLoad(20);
 
-                if (existingMatches.Any(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy")))
+                foreach (var entry in browser.FindElements(entries))
                 {
-                    var existingmatch = existingMatches.Single(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy"));
-                    existingmatch.Pick = browser.FindElement(matchPick).Text;
-                    existingmatch.Expert = browser.FindElement(expert).Text.Split(new string[] { "'" }, StringSplitOptions.None)[0].ToLower();
-                    existingmatch.Sport = sport.ToString();
+                    string[] title = entry.FindElements(entryTitle)[0].Text.Split(new string[] { "-", "NHL", "NBA", "NFL", "MBL", "Soccer" }, StringSplitOptions.None);
+                    title[0] = title[0].Trim();
+                    title[1] = title[1].Trim();
+                    matchList[0].MatchDttm.Date.ToString("MM/dd/yy");
+                    bool matchExsit = matchList.Any(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy"));
+
+                    if (matchExsit)
+                    {
+                        entriesLinks.Add(entry.FindElements(entryTitle)[0].GetAttribute("href"));
+                        existingMatches.Add(matchList.First(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy")));
+                    }
                 }
+
+                foreach (var link in entriesLinks)
+                {
+                    browser.Navigate().GoToUrl(link);
+                    string[] title = browser.FindElements(articleTitle)[0].Text.Split(new string[] { "-", "NHL", "NBA", "NFL", "MBL", "Soccer" }, StringSplitOptions.None);
+                    title[0] = title[0].Trim();
+                    title[1] = title[1].Trim();
+                    WaitForPageLoad(20);
+
+                    if (existingMatches.Any(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy")))
+                    {
+                        var existingmatch = existingMatches.Single(x => title[0].Contains(x.Local.Name) && title[0].Contains(x.Road.Name) && title[1] == x.MatchDttm.Date.ToString("MM/dd/yy"));
+                        existingmatch.Pick = browser.FindElement(matchPick).Text;
+                        existingmatch.Expert = browser.FindElement(expert).Text.Split(new string[] { "'" }, StringSplitOptions.None)[0].ToLower();
+                        existingmatch.Sport = sport.ToString();
+                    }
+                }
+
             }
+
             return existingMatches;
         }
     }
